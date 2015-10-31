851421a83565102149d7439674576efd3ce2e4a2
Priyank Verma <priyankvex95@gmail.com>
2015-10-31 12:43:06 +0530

    Add Sugar ORM.

---
+++ app/src/main/java/com/wordpress/priyankvex/practiceshapes/view/SelectShapeActivity.java
@@ -0,0 +1,11 @@
+package com.wordpress.priyankvex.practiceshapes.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by priyank on 31/10/15.
+ * Activity to select shape that has to be traced.
+ */
+public class SelectShapeActivity extends AppCompatActivity {
+
+}
