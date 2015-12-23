797a2796cd695812289ec05e8269e8484e5b6bc5
Priyank Verma <priyankvex95@gmail.com>
2015-12-23 14:02:48 +0530

    Add Settings Activity

---
+++ app/src/main/java/com/wordpress/priyankvex/practiceshapes/view/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.wordpress.priyankvex.practiceshapes.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by priyank on 23/12/15.
+ * Activity to provide settings for the app
+ */
+public class SettingsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
